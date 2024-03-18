b01dfe5f87d496fdb5a0ad03752a2e6ebd0552b3
ivanogor <ivanogor23@gmail.com>
2024-03-18 16:00:51 +0800

    Initial commit.

---
+++ src/main/java/pro/sky/mackitohomework/MackitoHomeworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.mackitohomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MackitoHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MackitoHomeworkApplication.class, args);
+    }
+
+}
